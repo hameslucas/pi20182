67c79d4153fb73e0d56010bc05e48761ba815208
hameslucas <45572541+hameslucas@users.noreply.github.com>
2018-12-03 17:04:27 -0200

    Update medico_flow.py

--- medico_flow.py
+++ medico_flow.py
@@ -1,1 +1,18 @@
+from errbot import botflow, FlowRoot, BotFlow, FLOW_END
 
+
+class ConsultaFlow(BotFlow):
+    'Agendamento de consulta'
+
+
+    @botflow
+    def agendamento(self, flow: FlowRoot):
+        'Consulta'
+        etapa1 = flow.connect('consulta', auto_trigger=True)
+        etapa2 = etapa1.connect('localizacao')
+        etapa3 = etapa2.connect('especialidade')
+        etapa4 = etapa3.connect('horario')
+
+        etapa2.connect(FLOW_END, predicate=lambda ctx: ctx['fim'] == True)
+        etapa3.connect(FLOW_END, predicate=lambda ctx: ctx['fim'] == True)
+        etapa4.connect(FLOW_END, predicate=lambda ctx: ctx['fim'] == True)
